6cef1414b580b27db135a9b57d868c2c7f439981
astonishiing <wagner.samuel@dinfor.uniceub>
2025-03-13 09:26:56 -0300

    criacao da classe ingrediente

---
+++ src/negocio/Ingredientes.java
@@ -0,0 +1,40 @@
+package negocio;
+
+public class Ingredientes {
+    private String nome;
+    private int quantidade;
+    private String unidade;
+    public Ingredientes(){
+        super();
+    }
+
+    public Ingredientes(String nome, int quantidade, String unidade) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.unidade = unidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+}
