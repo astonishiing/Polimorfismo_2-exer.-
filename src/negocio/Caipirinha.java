29625a90ad671fadb8e0a76affa8e34559442882
astonishiing <wagner.samuel@dinfor.uniceub>
2025-03-13 09:43:19 -0300

    criacao da classe Caipirinha

--- src/negocio/Caipirinha.java
+++ src/negocio/Caipirinha.java
@@ -1,4 +1,13 @@
 package negocio;
 
-public class Caipirinha {
+public class Caipirinha extends Drink implements Mixer{
+
+    @Override
+    public void misturar() {
+        super.adicionar(new Ingredientes("Cachaca", 100, "ml"));
+        super.adicionar(new Ingredientes("Limão", 3, "unidades"));
+        super.adicionar(new Ingredientes("Açucar", 5, "g"));
+        super.adicionar(new Ingredientes("Gelo", 8, "pedras"));
+
+    }
 }
