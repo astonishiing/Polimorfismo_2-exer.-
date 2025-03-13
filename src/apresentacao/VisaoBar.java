f9dcf5020147ccec1c07533a44207996127e784b
astonishiing <wagner.samuel@dinfor.uniceub>
2025-03-13 09:56:35 -0300

    criacao da classe VisaoBar

---
+++ src/apresentacao/VisaoBar.java
@@ -0,0 +1,12 @@
+package apresentacao;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class VisaoBar {
+    public static void main(String[] args) {
+        BufferedReader leitor = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
